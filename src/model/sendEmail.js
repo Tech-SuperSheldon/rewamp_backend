68b34c0191da9a088d475e76547e7ca5dc3c48de
Super Sheldon <tech@supersheldon.com>
2026-01-30 00:13:21 +0530

    Add submitFormData and bookingSummary fields

--- src/model/sendEmail.js
+++ src/model/sendEmail.js
@@ -7,6 +7,24 @@ const SendEmailSchema = new mongoose.Schema(
     mobile: { type: String, required: true, trim: true },
     childAgeYear: { type: String, required: true, trim: true },
     subject: { type: String, required: true, trim: true },
+    submitFormData: {
+      type: [
+        {
+          question: { type: String, trim: true, required: true },
+          answer: { type: String, trim: true, required: true },
+        },
+      ],
+      default: [],
+    },
+    bookingSummary: {
+      meetingDate: { type: String, trim: true },
+      startTimeUTC: { type: String, trim: true },
+      endTimeUTC: { type: String, trim: true },
+      timeZone: { type: String, trim: true },
+      startTimeIST: { type: String, trim: true },
+      endTimeIST: { type: String, trim: true },
+      meetingUrl: { type: String, trim: true },
+    },
     createdAt: { type: Date, default: Date.now },
   },
   { collection: "send_email" }
